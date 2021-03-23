daf76ae20c0b8d69ef035866292e19387b70ce38
Rayhaan Khalid <rkhalid@me.com>
2021-03-23 21:21:10 +0000

    move multiple elements off screen

--- App.js
+++ App.js
@@ -4,23 +4,51 @@ import { StyleSheet, View, Animated, Button } from 'react-native';
 
 export default function App() {
 
-  const opacity = useRef( new Animated.Value( 0 ) ).current
+  const moveS = useRef( new Animated.Value( 0 ) ).current
+  const moveP = useRef( new Animated.Value( -500 ) ).current
 
-  const handleClick = () => {
-    Animated.timing( opacity, {
-      toValue: 1,
-      duration: 1000,
+
+  const moveRight = () => {
+    Animated.timing( moveS, {
+      toValue: 500,
+      duration: 350,
+      useNativeDriver: true
+    }
+
+    ).start()
+
+    Animated.timing( moveP, {
+      toValue: 0,
+      duration: 350,
       useNativeDriver: true
     }
 
     ).start()
   }
 
+  const moveLeft = () => {
+    Animated.timing( moveS, {
+      toValue: 0,
+      duration: 350,
+      useNativeDriver: true
+    } ).start()
+
+    Animated.timing( moveP, {
+      toValue: -500,
+      duration: 350,
+      useNativeDriver: true
+    } ).start()
+  }
+
   return (
     <View style={ styles.container }>
-      <Animated.View style={ [ styles.animatedView, { opacity } ] }></Animated.View>
-      <Button title='Click me for animation please' onPress={ handleClick } />
-    </View>
+      <View style={ { borderWidth: 1 } }>
+        <Animated.View style={ [ styles.animatedView, { transform: [ { translateX: moveS } ] } ] }></Animated.View>
+        <Animated.View style={ [ styles.animatedView, { position: 'absolute', backgroundColor: 'peru', transform: [ { translateX: moveP } ] } ] }></Animated.View>
+      </View>
+      <Button title='Click me to move the view right' onPress={ moveRight } />
+      <Button title='Click me to move the view left' onPress={ moveLeft } />
+    </View >
   );
 }
 
@@ -34,6 +62,7 @@ const styles = StyleSheet.create( {
   animatedView: {
     backgroundColor: 'salmon',
     height: 100,
-    width: 100
+    width: 100,
+    position: 'relative'
   }
 } );
