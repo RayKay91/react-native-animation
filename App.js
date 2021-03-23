3c506ad0945ea98d554e6f0a5a00b03596f4f604
Rayhaan Khalid <rkhalid@me.com>
2021-03-23 20:54:45 +0000

    first animation ever!

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { StyleSheet, View, Animated, Button } from 'react-native';
 
 export default function App() {
+
+  const opacity = useRef( new Animated.Value( 0 ) ).current
+
+  const handleClick = () => {
+    Animated.timing( opacity, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true
+    }
+
+    ).start()
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ styles.container }>
+      <Animated.View style={ [ styles.animatedView, { opacity } ] }></Animated.View>
+      <Button title='Click me for animation please' onPress={ handleClick } />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create( {
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  animatedView: {
+    backgroundColor: 'salmon',
+    height: 100,
+    width: 100
+  }
+} );
