d20ce7a5cfc56120bcf9945a91cb753a18999bcf
Rayhaan Khalid <rkhalid@me.com>
2021-04-08 12:53:10 +0100

    pan gesture handler in reanimated

--- App.js
+++ App.js
@@ -1,44 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useRef, useEffect } from 'react';
-import { StyleSheet, View, Pressable, Text } from 'react-native';
-import Animated, { useSharedValue, useAnimatedStyle, withTiming, withSpring, interpolate } from 'react-native-reanimated'
+import Animated, {
+  useSharedValue,
+  withSpring,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+} from 'react-native-reanimated';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
 
+function App() {
+  const x = useSharedValue( 0 );
 
-export default function App() {
-
-  const opacity = useSharedValue( 0 )
+  const gestureHandler = useAnimatedGestureHandler( {
+    onStart: ( event, ctx ) => {
+      ctx.startX = x.value;
+    },
+    onActive: ( event, ctx ) => {
+      x.value = ctx.startX + event.translationX;
+    },
+    onEnd: () => {
+      x.value = withSpring( 0 );
+    },
+  } );
 
   const animatedStyle = useAnimatedStyle( () => {
     return {
-      opacity: opacity.value,
       transform: [
-        { scale: interpolate( opacity.value, [ 0, 1 ], [ 0.5, 1 ] ) }
-      ]
-    }
-  } )
-
-  useEffect( () => {
-    opacity.value = withSpring( 1 )
-  }, [] )
+        {
+          translateX: x.value,
+        },
+      ],
+    };
+  } );
 
   return (
-
-    <View style={ styles.container }>
-      <Animated.View style={ [ styles.square, animatedStyle ] }></Animated.View>
+    <View style={ { flex: 1, justifyContent: 'center', alignItems: 'center' } }>
+      <PanGestureHandler onGestureEvent={ gestureHandler }>
+        <Animated.View style={ [ styles.box, animatedStyle ] } />
+      </PanGestureHandler>
     </View>
-
   );
 }
 
+export default App
+
 const styles = StyleSheet.create( {
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  square: {
+  box: {
     height: 100,
     width: 100,
     backgroundColor: 'peru'
   }
-} );
+} )
